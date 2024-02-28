64de8ef54e477a20087aaaa8f8c23b7f0e93793f
saranyaraghava1 <727722euit166@skcet.ac.in>
2024-02-28 20:41:05 +0530

    commit

--- project/src/main/java/com/example/project/controller/SignUpController.java
+++ project/src/main/java/com/example/project/controller/SignUpController.java
@@ -1,6 +1,7 @@
 package com.example.project.controller;
 
 import java.util.List;
+import org.springframework.data.domain.Page;
 
 // import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +10,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.project.model.SignUp;
@@ -36,6 +37,15 @@ public class SignUpController {
     @GetMapping("/all")
     public List<SignUp> findA() {
         return obj.findA();
+    }
+
+    @GetMapping("/allPaginatedAndSorted")
+    public Page<SignUp> findAllPaginatedAndSorted(
+            @RequestParam(defaultValue = "0") int pageNo,
+            @RequestParam(defaultValue = "10") int pageSize,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "ASC") String direction) {
+        return obj.findAllPaginatedAndSorted(pageNo, pageSize, sortBy, direction);
     }
 
     @PutMapping("/update/{id}")
--- project/src/main/java/com/example/project/service/SignUpService.java
+++ project/src/main/java/com/example/project/service/SignUpService.java
@@ -3,6 +3,11 @@ package com.example.project.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
 import org.springframework.stereotype.Service;
 
 import com.example.project.model.SignUp;
@@ -42,4 +47,9 @@ public class SignUpService {
 
     }
 
+    public Page<SignUp> findAllPaginatedAndSorted(int pageNo, int pageSize, String sortBy, String direction) {
+        Sort sort = Sort.by(Sort.Direction.fromString(direction), sortBy);
+        Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
+        return repo.findAll(pageable);
+    }
 }
