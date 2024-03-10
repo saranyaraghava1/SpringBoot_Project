812e7ae7e9043a7e66cd66bfac1921a658146bf2
saranyaraghava1 <727722euit166@skcet.ac.in>
2024-03-10 20:49:22 +0530

    Commit

--- project/src/main/java/com/example/project/ProjectApplication.java
+++ project/src/main/java/com/example/project/ProjectApplication.java
@@ -1,14 +1,9 @@
 package com.example.project;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
-
-
-
 @OpenAPIDefinition(
 		info =@Info(
 				title = "App Title",
